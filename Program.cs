811a204ca9ad4150f82eee31161d012acf4ac5f7
DavidRicardoJ <david_ricardo@live.com>
2019-06-22 15:15:02 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using inheritance4_CSharp.entities;
+using System.Globalization;
+
+namespace inheritance4_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employee: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"\nEmployee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char r = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (r == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double addCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutSourceEmployee(name, hours, valuePerHour, addCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Payments:");
+            foreach (Employee item in list)
+            {
+                Console.WriteLine(item.Name + " - $"+ item.Payment().ToString("F2",CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}
---
+++ entities/OutSourceEmployee.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace inheritance4_CSharp.entities
+{
+    class OutSourceEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutSourceEmployee() { }
+
+        public OutSourceEmployee(string name, int hours, double valuePerHour, double additionalCharge) :base (name,hours,valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + AdditionalCharge * 1.10;
+        }
+
+    }
+}
